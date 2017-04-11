db37d1dc79a0e8329d6132599d0588338c392e28
dave <dave@agileinf.com>
2017-04-12 04:54:10 +0530

    refactoring update and insert method with blank insert and atomic update

--- Controllers/MaintainBusinessRulesController.py
+++ Controllers/MaintainBusinessRulesController.py
@@ -3,119 +3,158 @@ from flask_restful import Resource
 from Helpers.DatabaseHelper import DatabaseHelper
 import csv
 from Constants.Status import *
+
+
 class MaintainBusinessRulesController(Resource):
-     def get(self, business_rule=None):
-        if business_rule :
-             return self.render_business_rule_json(business_rule)
-        return self.render_business_rules_json()
 
-     def post(self):
-         br = request.get_json(force=True)
-         res=self.insert_business_rules(br)
-         return res
+	def get(self, business_rule=None):
+		if business_rule:
+			return self.render_business_rule_json(business_rule)
+		return self.render_business_rules_json()
 
-     def put(self, business_rule=None):
-         if(business_rule == None):
-             return BUSINESS_RULE_EMPTY
-         br=request.get_json(force=True)
-         res=self.update_business_rules(br, business_rule)
-         return res
+	def post(self):
+		br = request.get_json(force=True)			
+		res = self.insert_business_rules(br)
+		return res
 
-     def delete(self, business_rule=None):
-         if(business_rule == None):
-             return BUSINESS_RULE_EMPTY
-         res=self.delete_business_rules(business_rule)
-         return res
+	def put(self, id=None):
+		if id == None:
+			return BUSINESS_RULE_EMPTY
+		br = request.get_json(force=True)
+		res = self.update_business_rules(br, id)
+		return res
 
-     def render_business_rules_json(self):
-        db=DatabaseHelper()
+	def delete(self, business_rule=None):
+		if business_rule == None:
+			return BUSINESS_RULE_EMPTY
+		res = self.delete_business_rules(business_rule)
+		return res
 
-        business_rules_dict={}
-        business_rules_list=[]
-        cur = db.query("select * from business_rules")
-        business_rules = cur.fetchall()
+	def render_business_rules_json(self):
+		db = DatabaseHelper()
 
-        cols=[i[0] for i in cur.description]
-        business_rules_dict['cols']=cols
-        # print(business_rules_dict)
+		business_rules_dict = {}
+		business_rules_list = []
+		cur = db.query('select * from business_rules')
+		business_rules = cur.fetchall()
 
-        for br in business_rules:
-            business_rules_list.append(br)
+		cols = [i[0] for i in cur.description]
+		business_rules_dict['cols'] = cols
 
-        business_rules_dict['rows']=business_rules_list
+		# print(business_rules_dict)
 
-        json_dump=(business_rules_dict)
+		for br in business_rules:
+			business_rules_list.append(br)
 
-        return json_dump
-     def render_business_rule_json(self, business_rule):
-        db=DatabaseHelper()
-        query = "select * from business_rules where business_rule = %s"
-        cur = db.query(query, (business_rule, ))
-        data = cur.fetchone()
-        if data :
-            return data
-        return NO_BUSINESS_RULE_FOUND
+		business_rules_dict['rows'] = business_rules_list
 
-     def insert_business_rules(self,br):
-         db=DatabaseHelper()
-         sql= "insert into business_rules(rule_execution_order,business_rule,source_id,rule_description,\
-                        logical_condition,data_fields_list,python_implementation,business_or_validation,\
-                    rule_type,valid_from,valid_to) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+		json_dump = business_rules_dict
 
-         params= tuple((br["rule_execution_order"],br["business_rule"],br["source_id"],br["rule_description"], \
-                     br["logical_condition"],br["data_fields_list"],br["python_implementation"],br["business_or_validation"], \
-                     br["rule_type"],br["valid_from"],br["valid_to"]))
+		return json_dump
 
-         res=db.transact(sql,params)
-         if res == 0 :
-             return self.render_business_rule_json(br["business_rule"])
-         return UPDATE_ERROR
+	def render_business_rule_json(self, business_rule):
+		db = DatabaseHelper()
+		query = 'select * from business_rules where business_rule = %s'
+		cur = db.query(query, (business_rule, ))
+		data = cur.fetchone()
+		if data:
+			return data
+		return NO_BUSINESS_RULE_FOUND
 
-     def update_business_rules(self,br,business_rule):
-         db=DatabaseHelper()
-         sql="Update business_rules set " \
-             "rule_execution_order=%s, \
-             business_rule=%s,\
-             source_id=%s,\
-             rule_description=%s,\
-             logical_condition=%s,\
-             data_fields_list=%s,\
-             python_implementation=%s,\
-             business_or_validation=%s,\
-             rule_type=%s,\
-             valid_from=%s,\
-             valid_to=%s where business_rule=%s"
+	def insert_business_rules(self, br):
+		db = DatabaseHelper()
+		sql = \
+			"insert into business_rules(rule_execution_order,business_rule,source_id,rule_description,\
+						logical_condition,data_fields_list,python_implementation,business_or_validation,\
+					rule_type,valid_from,valid_to) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
-         params=(br["rule_execution_order"],br["business_rule"],br["source_id"],br["rule_description"], \
-                     br["logical_condition"],br["data_fields_list"],br["python_implementation"],br["business_or_validation"], \
-                     br["rule_type"],br["valid_from"],br["valid_to"],business_rule, )
+		if bool(br):
+			params = tuple((
+				br['rule_execution_order'],
+				br['business_rule'],
+				br['source_id'],
+				br['rule_description'],
+				br['logical_condition'],
+				br['data_fields_list'],
+				br['python_implementation'],
+				br['business_or_validation'],
+				br['rule_type'],
+				br['valid_from'],
+				br['valid_to'],
+				))
+		else:
+			params = tuple((
+				0,
+				'null',
+				'null',
+				'null',
+				'null',
+				'null',
+				'null',
+				'null',
+				'null',
+				'null',
+				'null',
+				))
 
-         res=db.transact(sql,params)
-         if(res == 0):
-             return self.render_business_rule_json(business_rule)
-         return UPDATE_ERROR
-     def delete_business_rules(self,business_rule):
-         db=DatabaseHelper()
-         sql="delete from business_rules where business_rule=%s"
-         params=(business_rule,)
-         res=db.transact(sql,params)
-         return res
+		res = db.transact(sql, params)
+		if res:
+			return {"id":res}
+		return UPDATE_ERROR
 
-     def export_to_csv(self,source_id='ALL'):
-         db=DatabaseHelper()
+	def update_business_rules(self, br, id):
+		db = DatabaseHelper()
+		sql = \
+			'Update business_rules set rule_execution_order=%s,              business_rule=%s,             source_id=%s,             rule_description=%s,             logical_condition=%s,             data_fields_list=%s,             python_implementation=%s,             business_or_validation=%s,             rule_type=%s,             valid_from=%s,             valid_to=%s where id=%s'
 
-         sql="select * from business_rules where 1=1"
+		params = (
+			br['rule_execution_order'],
+			br['business_rule'],
+			br['source_id'],
+			br['rule_description'],
+			br['logical_condition'],
+			br['data_fields_list'],
+			br['python_implementation'],
+			br['business_or_validation'],
+			br['rule_type'],
+			br['valid_from'],
+			br['valid_to'],
+			id,
+			)
 
-         if source_id!='ALL':
-             sql+=" and source_id='"+str(source_id)+"'"
+		res = db.transact(sql, params)
+		if res == 0:
+			return {"id":res}
+		return UPDATE_ERROR
 
-         cur=db.query(sql)
+	def delete_business_rules(self, business_rule):
+		db = DatabaseHelper()
+		sql = 'delete from business_rules where business_rule=%s'
+		params = (business_rule, )
+		res = db.transact(sql, params)
+		return res
 
-         business_rules=cur.fetchall()
-         #keys=business_rules[0].keys()
-         keys=[i[0] for i in cur.description]
+	def export_to_csv(self, source_id='ALL'):
+		db = DatabaseHelper()
 
-         with open("business_rules.csv","wt") as output_file:
-             dict_writer=csv.DictWriter(output_file,keys)
-             dict_writer.writeheader()
-             dict_writer.writerows(business_rules)
+		sql = 'select * from business_rules where 1=1'
+
+		if source_id != 'ALL':
+			sql += " and source_id='" + str(source_id) + "'"
+
+		cur = db.query(sql)
+
+		business_rules = cur.fetchall()
+
+		 # keys=business_rules[0].keys()
+
+		keys = [i[0] for i in cur.description]
+
+		with open('business_rules.csv', 'wt') as output_file:
+			dict_writer = csv.DictWriter(output_file, keys)
+			dict_writer.writeheader()
+			dict_writer.writerows(business_rules)
+
+
+
+			
--- api.py
+++ api.py
@@ -20,7 +20,7 @@ api.add_resource(MaintainBusinessRulesController,
     endpoint="business_rules_ep"
 )
 api.add_resource(MaintainBusinessRulesController,
-    apiPath + "/business-rules/<string:business_rule>",
+    apiPath + "/business-rules/<string:id>",
     endpoint="business_rule_ep"
 )
 if __name__ == '__main__':
