e7d43ffcef1c37d09ae60ab41c0f18a23c4985d5
suchibrata <mailto.dutta.joy@gmail.com>
2017-04-17 20:11:11 +0530

    Added api functions for business rule lookup in reports

--- Controllers/MaintainBusinessRulesController.py
+++ Controllers/MaintainBusinessRulesController.py
@@ -1,14 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource
-from Helpers.DatabaseHelper import DatabaseHelper
-from Helpers.authenticate import *
+from RegOpzAPI.Helpers.DatabaseHelper import DatabaseHelper
 import csv
 from Constants.Status import *
 class MaintainBusinessRulesController(Resource):
-     @authenticate
-     def get(self, business_rule=None):
-        if business_rule :
-             return self.render_business_rule_json(business_rule)
+     def get(self, id=None):
+        if id :
+             return self.render_business_rule_json(id)
         return self.render_business_rules_json()
 
      def post(self):
@@ -16,17 +14,17 @@ class MaintainBusinessRulesController(Resource):
          res=self.insert_business_rules(br)
          return res
 
-     def put(self, business_rule=None):
-         if(business_rule == None):
+     def put(self, id=None):
+         if(id == None):
              return BUSINESS_RULE_EMPTY
          br=request.get_json(force=True)
-         res=self.update_business_rules(br, business_rule)
+         res=self.update_business_rules(br, id)
          return res
 
-     def delete(self, business_rule=None):
-         if(business_rule == None):
+     def delete(self, id=None):
+         if(id == None):
              return BUSINESS_RULE_EMPTY
-         res=self.delete_business_rules(business_rule)
+         res=self.delete_business_rules(id)
          return res
 
      def render_business_rules_json(self):
@@ -49,10 +47,11 @@ class MaintainBusinessRulesController(Resource):
         json_dump=(business_rules_dict)
 
         return json_dump
-     def render_business_rule_json(self, business_rule):
+
+     def render_business_rule_json(self, id):
         db=DatabaseHelper()
-        query = "select * from business_rules where business_rule = %s"
-        cur = db.query(query, (business_rule, ))
+        query = "select * from business_rules where id = %s"
+        cur = db.query(query, (id, ))
         data = cur.fetchone()
         if data :
             return data
@@ -60,20 +59,28 @@ class MaintainBusinessRulesController(Resource):
 
      def insert_business_rules(self,br):
          db=DatabaseHelper()
-         sql= "insert into business_rules(rule_execution_order,business_rule,source_id,rule_description,\
-                        logical_condition,data_fields_list,python_implementation,business_or_validation,\
-                    rule_type,valid_from,valid_to) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
-         params= tuple((br["rule_execution_order"],br["business_rule"],br["source_id"],br["rule_description"], \
+         sql = "insert into business_rules(rule_execution_order,business_rule,source_id,rule_description,\
+                                 logical_condition,data_fields_list,python_implementation,business_or_validation,\
+                             rule_type,valid_from,valid_to) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+         if br:
+           params= tuple((br["rule_execution_order"],br["business_rule"],br["source_id"],br["rule_description"], \
                      br["logical_condition"],br["data_fields_list"],br["python_implementation"],br["business_or_validation"], \
                      br["rule_type"],br["valid_from"],br["valid_to"]))
 
+         else:
+           params=(None,None,None,None,None,None,None,None,None,None,None)
+
          res=db.transact(sql,params)
-         if res == 0 :
-             return self.render_business_rule_json(br["business_rule"])
+         print("Result" +str(res))
+
+         if res > 0 :
+          return self.render_business_rule_json(res)
          return UPDATE_ERROR
 
-     def update_business_rules(self,br,business_rule):
+
+     def update_business_rules(self,br,id):
          db=DatabaseHelper()
          sql="Update business_rules set " \
              "rule_execution_order=%s, \
@@ -86,20 +93,22 @@ class MaintainBusinessRulesController(Resource):
              business_or_validation=%s,\
              rule_type=%s,\
              valid_from=%s,\
-             valid_to=%s where business_rule=%s"
+             valid_to=%s where id=%s"
 
          params=(br["rule_execution_order"],br["business_rule"],br["source_id"],br["rule_description"], \
                      br["logical_condition"],br["data_fields_list"],br["python_implementation"],br["business_or_validation"], \
-                     br["rule_type"],br["valid_from"],br["valid_to"],business_rule, )
+                     br["rule_type"],br["valid_from"],br["valid_to"],id )
 
          res=db.transact(sql,params)
+         print("Result "+str(res))
          if(res == 0):
-             return self.render_business_rule_json(business_rule)
+             return self.render_business_rule_json(id)
          return UPDATE_ERROR
-     def delete_business_rules(self,business_rule):
+
+     def delete_business_rules(self,id):
          db=DatabaseHelper()
-         sql="delete from business_rules where business_rule=%s"
-         params=(business_rule,)
+         sql="delete from business_rules where id=%s"
+         params=(id,)
          res=db.transact(sql,params)
          return res
 
@@ -121,3 +130,49 @@ class MaintainBusinessRulesController(Resource):
              dict_writer=csv.DictWriter(output_file,keys)
              dict_writer.writeheader()
              dict_writer.writerows(business_rules)
+
+     def list_reports_for_rule(**kwargs):
+
+         parameter_list = ['business_rule']
+
+         if set(parameter_list).issubset(set(kwargs.keys())):
+             business_rule = kwargs['business_rule']
+
+         else:
+             print("Please supply parameters: " + str(parameter_list))
+             sys.exit(1)
+
+         db = DatabaseHelper()
+         sql = "select distinct report_id,sheet_id,cell_id from report_calc_def \
+                where cell_business_rules like  '%" + business_rule + "%'"
+         cur=db.query(sql)
+         report_list = cur.fetchall()
+
+         return [(rpt['report_id'], rpt['sheet_id'], rpt['cell_id']) for rpt in report_list]
+
+     def list_reports_for_rule_list(**kwargs):
+
+         parameter_list = ['business_rule_list']
+
+         if set(parameter_list).issubset(set(kwargs.keys())):
+             business_rule_list = kwargs['business_rule_list']
+         else:
+             print("Please supply parameters: " + str(parameter_list))
+             sys.exit(1)
+
+         db=DatabaseHelper()
+         sql = "select report_id,sheet_id,cell_id,cell_business_rules from report_calc_def"
+         cur=db.query(sql)
+         data_list = cur.fetchall()
+
+         result_set = []
+         for data in data_list:
+             # print(data['cell_business_rules'])
+             cell_rule_list = data['cell_business_rules'].split(',')
+             # print(type(cell_rule_list))
+             if set(business_rule_list).issubset(set(cell_rule_list)):
+                 # print(data)
+                 result_set.append(data)
+
+         return result_set
+
