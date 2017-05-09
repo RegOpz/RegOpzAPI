5db949c22fbc5a842ae6377232b64d5a81f17235
suchibrata <mailto.dutta.joy@gmail.com>
2017-05-09 15:17:06 +0530

    Added API for render_report drilldown etc

--- Controllers/DocumentController.py
+++ Controllers/DocumentController.py
@@ -11,6 +11,7 @@ import openpyxl as xls
 import Helpers.utils as util
 from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment, Protection
 from openpyxl.utils import get_column_letter
+import Helpers.utils as util
 import json
 UPLOAD_FOLDER = './uploads/templates'
 ALLOWED_EXTENSIONS = set(['txt', 'xls', 'xlsx'])
@@ -23,7 +24,7 @@ class DocumentController(Resource):
             return (document.get(doc_id))
         return (document.get())"""
         self.report_id = doc_id
-        return self.render_report_template_json()
+        return self.render_report_json('20160930')
     def post(self):
         if 'file' not in request.files:
             return NO_FILE_SELECTED
@@ -105,6 +106,94 @@ class DocumentController(Resource):
         print('====================================')
         return 0
 
+    def render_report_json(self,reporting_date,cell_format_yn='Y'):
+
+        db = DatabaseHelper()
+
+        cur = db.query("select distinct sheet_id from report_def where report_id=%s", (self.report_id,))
+        sheets = cur.fetchall()
+        print(sheets)
+
+        sheet_d_list = []
+        for sheet in sheets:
+            matrix_list = []
+            row_attr = {}
+            col_attr = {}
+            cur = db.query(
+                "select cell_id,cell_render_def,cell_calc_ref from report_def where report_id=%s and sheet_id=%s",
+                (self.report_id, sheet["sheet_id"]))
+            report_template = cur.fetchall()
+
+            data = db.query('select b.report_id,b.sheet_id,b.cell_id,a.cell_summary,\
+                                b.reporting_scale,b.rounding_option \
+                                from report_calc_def b left join report_summary a\
+                                on a.report_id=b.report_id and\
+                                a.sheet_id=b.sheet_id and \
+                                a.cell_id=b.cell_id and \
+                                a.reporting_date=%s \
+                                where b.report_id=%s \
+                                and b.sheet_id=%s\
+                                order by b.report_id,b.sheet_id,b.cell_id', (reporting_date, self.report_id,sheet["sheet_id"])).fetchall()
+
+            for row in report_template:
+                cell_d = {}
+                if row["cell_render_def"] == 'STATIC_TEXT':
+                    cell_d['cell'] = row['cell_id']
+                    cell_d['value'] = row['cell_calc_ref']
+                    matrix_list.append(cell_d)
+
+
+                elif row['cell_render_def'] == 'MERGED_CELL':
+                    start_cell, end_cell = row['cell_id'].split(':')
+                    cell_d['cell'] = start_cell
+                    cell_d['value'] = row['cell_calc_ref']
+                    cell_d['merged'] = end_cell
+                    matrix_list.append(cell_d)
+
+
+                elif row['cell_render_def'] == 'ROW_HEIGHT':
+                    if row['cell_calc_ref'] == 'None':
+                        row_height = '12.5'
+                    else:
+                        row_height = row['cell_calc_ref']
+                    row_attr[row['cell_id']] = {'height': row_height}
+
+
+                elif row['cell_render_def'] == 'COLUMN_WIDTH':
+                    if row['cell_calc_ref'] == 'None':
+                        col_width = '13.88'
+                    else:
+                        col_width = row['cell_calc_ref']
+                    col_attr[row['cell_id']] = {'width': col_width}
+
+            for row in data:
+                cell_d={}
+                if cell_format_yn == 'Y':
+                    # print(row["cell_id"],row["cell_summary"])
+                    cell_summary = util.round_value(
+                        float(util.if_null_zero(row["cell_summary"])) / float(row["reporting_scale"]),
+                        row["rounding_option"])
+
+                else:
+                    cell__summary= float(util.if_null_zero(row["cell_summary"]))
+
+                cell_d['cell']=row['cell_id']
+                cell_d['value']=cell_summary
+                matrix_list.append(cell_d)
+
+
+            sheet_d = {}
+            sheet_d['sheet'] = sheet['sheet_id']
+            # print(sheet_d['sheet'])
+            sheet_d['matrix'] = matrix_list
+            sheet_d['row_attr'] = row_attr
+            sheet_d['col_attr'] = col_attr
+            sheet_d_list.append(sheet_d)
+
+        json_dump = (sheet_d_list)
+        # print(json_dump)
+        return json_dump
+
     def render_report_template_json(self):
 
         db = DatabaseHelper()
@@ -138,7 +227,7 @@ class DocumentController(Resource):
                     matrix_list.append(cell_d)
 
                 elif row['cell_render_def']=='ROW_HEIGHT':
-                    if row['cell_calc_ref']=="None":
+                    if row['cell_calc_ref']==None:
                         row_height=12.5
                     else:
                         row_height=row['cell_calc_ref']
@@ -146,7 +235,7 @@ class DocumentController(Resource):
                     row_attr[row['cell_id']]={'height':row_height}
 
                 elif row['cell_render_def'] == 'COLUMN_WIDTH':
-                    if row['cell_calc_ref'] == "None":
+                    if row['cell_calc_ref'] == None:
                         col_width = 13.88
                     else:
                         col_width = row['cell_calc_ref']
@@ -165,3 +254,215 @@ class DocumentController(Resource):
         json_dump = (sheet_d_list)
         #print(json_dump)
         return json_dump
+
+    def render_data_load_dates(self,start_reporting_date='19000101',end_reporting_date='39991231'):
+
+        month_lookup={ '01': 'January',
+                       '02':'February',
+                       '03':'March',
+                       '04':'April',
+                       '05':'May',
+                       '06':'June',
+                       '07':'July',
+                       '08':'August',
+                       '09':'Sepember',
+                       '10':'October',
+                       '11':'November',
+                       '12':'December'
+                       }
+        db = DatabaseHelper()
+
+        catalog=db.query("select distinct reporting_date from report_catalog where reporting_date between "+ start_reporting_date + " and " + end_reporting_date + " order by reporting_date").fetchall()
+
+        catalog_list=[]
+
+        for cat in catalog:
+            year=cat['reporting_date'][:4]
+            month_num=cat['reporting_date'][4:6]
+            report_date=cat['reporting_date'][6:]
+            month=month_lookup[month_num]
+
+            #print(year,month,bus_date)
+            #print(list(map(itemgetter('year'),catalog_list)))
+
+            idx=list(map(itemgetter('year'),catalog_list)).index(year)\
+                if year in map(itemgetter('year'),catalog_list) else None
+            #print(list(map(itemgetter('year'), catalog_list)))
+            if idx==None:
+                d={'year':year,'month':{month:[report_date]}}
+                catalog_list.append(d)
+                #print(catalog_list)
+
+            else:
+                if month in catalog_list[idx]['month'].keys():
+                    catalog_list[idx]['month'][month].append(report_date)
+                else:
+                    catalog_list[idx]['month'][month]=[report_date]
+
+
+        return (catalog_list)
+
+    def render_report_list(self,**kwargs):
+
+        parameter_list = ['reporting_date']
+
+        if set(parameter_list).issubset(set(kwargs.keys())):
+            reporting_date = kwargs['reporting_date']
+        else:
+            print("Please supply parameters: " + str(parameter_list))
+
+        db=DatabaseHelper()
+        reports = db.query("select report_id\
+                        from report_catalog where reporting_date='"+reporting_date+"'").fetchall()
+
+        #print(data_sources)
+        return (reports)
+
+
+    def export_to_excel(self, **kwargs):
+        parameter_list = ['reporting_date','cell_format_yn']
+        if set(parameter_list).issubset(set(kwargs.keys())):
+            report_id = self.report_id
+            reporting_date=kwargs['reporting_date']
+            cell_format_yn=kwargs['cell_format_yn']
+            target_file_name = report_id+ '_' + reporting_date + '.xlsx'
+        else:
+            print("Please supply parameters: " + str(parameter_list))
+
+        #Create report template
+        wr = xls.Workbook()
+        # target_dir='../output/'
+        target_dir = './'
+        db=DatabaseHelper()
+
+        sheets=db.query('select distinct sheet_id from report_def where report_id=%s', (report_id,)).fetchall()
+
+        # print sheets
+        # The default sheet of the workbook
+        al = Alignment(horizontal="center", vertical="center", wrap_text=True, shrink_to_fit=True)
+        ws = wr.worksheets[0]
+        for sheet in sheets:
+            # The first sheet title will be Sheet, so do not create any sheet, just rename the title
+            if ws.title == 'Sheet':
+                ws.title = sheet["sheet_id"]
+            else:
+                ws = wr.create_sheet(title=sheet["sheet_id"])
+
+            report_template=db.query('select cell_id,cell_render_def,cell_calc_ref from report_def where report_id=? and sheet_id=?',
+                        (report_id, sheet["sheet_id"])).fetchall()
+
+            for row in report_template:
+                if row["cell_render_def"] == 'MERGED_CELL':
+                    ws.merge_cells(row["cell_id"])
+                    startcell, endcell = row["cell_id"].split(':')
+                    ws[startcell].value = row["cell_calc_ref"]
+                    ws[startcell].value = row["cell_calc_ref"]
+                    ws[startcell].fill = PatternFill("solid", fgColor="DDDDDD")
+                    ws[startcell].font = Font(size=9)
+                    ws[startcell].alignment = al
+                elif row["cell_render_def"] == 'STATIC_TEXT':
+                    ws[row["cell_id"]].value = row["cell_calc_ref"]
+                    ws[row["cell_id"]].fill = PatternFill("solid", fgColor="DDDDDD")
+                    ws[row["cell_id"]].font = Font(size=9)
+                    if row["cell_calc_ref"][:1] != '=' and row["cell_calc_ref"][
+                                                           len(row["cell_calc_ref"]) - 1:1] != '%' and 'SUM' not in row[
+                        "cell_calc_ref"]:
+                        ws[row["cell_id"]].alignment = al
+                elif row["cell_render_def"] == 'CALCULATE_FORMULA':
+                    ws[row["cell_id"]].fill = PatternFill("solid", fgColor="DDDDDD")
+                    ws[row["cell_id"]].font = Font(bold=True, size=9)
+                    if '=' in row["cell_calc_ref"]:
+                        ws[row["cell_id"]].value = row["cell_calc_ref"]
+                    else:
+                        ws[row["cell_id"]].value = '=' + row["cell_calc_ref"]
+                if row["cell_render_def"] == 'ROW_HEIGHT' and row["cell_calc_ref"] != 'None':
+                    # print('row ' + row["cell_id"])
+                    ws.row_dimensions[int(row["cell_id"])].height = float(row["cell_calc_ref"])
+                elif row["cell_render_def"] == 'COLUMN_WIDTH' and row["cell_calc_ref"] != 'None':
+                    # print(row["cell_id"])
+                    ws.column_dimensions[row["cell_id"]].width = float(row["cell_calc_ref"])
+                else:
+                    pass
+
+        wr.save(target_dir + target_file_name)
+        #End create report template
+
+        #Create report body
+        wb = xls.load_workbook(target_dir + report_file_name)
+        data=db.query('select b.report_id,b.sheet_id,b.cell_id,a.cell_summary,\
+                    b.reporting_scale,b.rounding_option \
+                    from report_calc_def b left join report_summary a\
+                    on a.report_id=b.report_id and\
+                    a.sheet_id=b.sheet_id and \
+                    a.cell_id=b.cell_id and \
+                    a.reporting_date=%s \
+                    where b.report_id=%s \
+                    order by b.report_id,b.sheet_id,b.cell_id',(reporting_date,report_id,)).fetchall()
+
+        for row in data:
+            ws = wb.get_sheet_by_name(row["sheet_id"])
+            if cell_format_yn == 'Y':
+                # print(row["cell_id"],row["cell_summary"])
+                cell_summary = util.round_value(
+                    float(util.if_null_zero(row["cell_summary"])) / float(row["reporting_scale"]),
+                    row["rounding_option"])
+                ws[row["cell_id"]].value = cell_summary
+            else:
+                ws[row["cell_id"]].value = float(util.if_null_zero(row["cell_summary"]))
+
+            ws[row["cell_id"]].font = Font(size=9)
+
+
+        wb.save(target_dir + target_file_name)
+
+        #End create report body
+
+        return
+
+    def cell_drill_down_rules(self,**kwargs):
+        parameter_list = ['report_id', 'sheet_id', 'cell_id']
+        if set(parameter_list).issubset(set(kwargs.keys())):
+            report_id = kwargs["report_id"]
+            sheet_id = kwargs['sheet_id']
+            cell_id = kwargs['cell_id']
+        else:
+            print("Please supply parameters: " + str(parameter_list))
+
+        db=DatabaseHelper()
+
+        sql = "select b.source_table_name, a.* from report_calc_def a,\
+            data_source_information b where a.source_id=b.source_id and \
+            report_id='" + report_id + "' and sheet_id='" + sheet_id + "' and \
+            cell_id='" + cell_id + "'"
+
+        cell_rules=db.query(sql).fetchall()
+
+        return [(tpl['source_table_name'], tpl['source_id'], tpl['cell_business_rules']) for tpl in cell_rules]
+
+    def cell_drill_down_data(self,**kwargs):
+
+        parameter_list = ['source_id', 'report_id', 'sheet_id', 'cell_id', 'reporting_date']
+
+        if set(parameter_list).issubset(set(kwargs.keys())):
+            source_id = kwargs["source_id"]
+            report_id = kwargs["report_id"]
+            sheet_id = kwargs['sheet_id']
+            cell_id = kwargs['cell_id']
+            reporting_date = kwargs['reporting_date']
+        else:
+            print("Please supply parameters: " + str(parameter_list))
+
+        db=DatabaseHelper()
+
+        src_inf=db.query("select * from data_source_information where source_id=" + str(source_id)).fetchone()
+
+        key_column = util.get_keycolumn(cur, src_inf['source_table_name'])
+
+        sql = "select a.*,b.* from " + src_inf['source_table_name'] + " a, report_qualified_data_link b\
+             where a." + key_column + "=b.qualifying_key and b.report_id='" + report_id + "' and b.source_id='" + str(
+            source_id) + \
+              "' and b.sheet_id='" + sheet_id + "' and b.cell_id='" + cell_id + "' and b.reporting_date='" + reporting_date + "'"
+
+        drill_data = db.query(sql).fetchall()
+
+        return drill_data
