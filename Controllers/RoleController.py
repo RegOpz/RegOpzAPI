e602e398152b89e6604500c9fe2859d3f69559fb
Progyan Bhattacharya <progyanb@acm.org>
2017-07-17 12:33:35 +0530

    RoleModel: Modified API for saving advantage
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- Controllers/RoleController.py
+++ Controllers/RoleController.py
@@ -1,15 +1,14 @@
-from Helpers.DatabaseHelper import DatabaseHelper
 from flask_restful import Resource,request
 from Models.UserPermission import UserPermission
 
 class RoleController(Resource):
-
     def get(self):
-        return UserPermission().get()
+        role = request.args.get('role')
+        return UserPermission().get(role)
 
     def post(self):
         res = request.get_json(force=True)
         return UserPermission().save(res)
 
-    def delete(self, role=None):
+    def delete(self, role = None):
         return UserPermission().remove(role)
--- Models/UserPermission.py
+++ Models/UserPermission.py
@@ -5,28 +5,36 @@ class UserPermission(object):
     def __init__(self):
         self.dbhelper = DatabaseHelper()
 
-    def get(self):
-        queryString_1 = "SELECT role FROM roles"
-        roleQuery = self.dbhelper.query(queryString_1)
-        roles = roleQuery.fetchall()
-        if len(roles) == 0:
-            return ROLE_EMPTY
-        queryString_2 = "SELECT component, permission FROM roles JOIN (permissions, components, permission_def) ON\
+    def get(self, roleId = None):
+        queryString = "SELECT component, permission FROM roles JOIN (permissions, components, permission_def) ON\
             (roles.id = permissions.role_id AND permissions.component_id = components.id AND permissions.permission_id = permission_def.id)\
             WHERE roles.role=%s"
-        dataList = []
-        for role in roles:
-            queryParams = (role['role'], )
-            permQuery = self.dbhelper.query(queryString_2, queryParams)
-            permissions = permQuery.fetchall()
-            data = {
-                'role': role['role'],
-                'permissions': permissions
-            }
-            dataList.append(data)
-        if len(dataList) == 0:
-            return PERMISSION_EMPTY
-        return dataList
+        if roleId:
+            queryParams = (roleId, )
+            cur = self.dbhelper.query(queryString, queryParams)
+            data = cur.fetchall()
+            if len(data) == 0:
+                return PERMISSION_EMPTY
+            return data
+        else:
+            queryString_1 = "SELECT role FROM roles"
+            roleQuery = self.dbhelper.query(queryString_1)
+            roles = roleQuery.fetchall()
+            if len(roles) == 0:
+                return ROLE_EMPTY
+            dataList = []
+            for role in roles:
+                queryParams = (role['role'], )
+                permQuery = self.dbhelper.query(queryString, queryParams)
+                permissions = permQuery.fetchall()
+                data = {
+                    'role': role['role'],
+                    'permissions': permissions
+                }
+                dataList.append(data)
+            if len(dataList) == 0:
+                return PERMISSION_EMPTY
+            return dataList
 
     def obtain(self, userId=None):
         if userId:
@@ -50,35 +58,67 @@ class UserPermission(object):
     def save(self, entry = None):
         if entry:
             self.role = entry['role']
-            self.permission = entry['permission']
-            if len(self.permission) == 0:
+            self.permissions = entry['permissions']
+            if len(self.permissions) == 0:
                 return PERMISSION_EMPTY
-            queryString = "SELECT id from roles where role=%s"
-            queryParams_1 = (self.role, )
-            cur = self.dbhelper.query(queryString, queryParams_1)
-            data = cur.fetchone()
-            if data:
-                rowId = data['id']
-            else:
-                queryString_1 = "INSERT INTO roles (role) VALUES(%s)"
-                try:
-                    rowId = self.dbhelper.transact(queryString_1, queryParams_1)
-                except Exception:
-                    return { "msg": "Failed to create role " + self.role },403
-            queryString_2 = "INSERT INTO permissions (role_id, component_id, permission) VALUES \
-                (%s,(SELECT id FROM components WHERE component=%s),%s) ON DUPLICATE KEY UPDATE permission=%s"
-            queryParams_2 = []
-            for comp, perm in self.permission.items():
-                queryParams_2.append( (rowId, comp, perm, perm, ) )
             try:
-                if len(self.permission) > 1:
-                    lastRowId = self.dbhelper.transactmany(queryString_2, queryParams_2)
+                roleId = self.getRoleId()
+            except ValueError as e:
+                print("Error in Get Role", e)
+                return { "msg": "Failed to create role " + self.role },403
+            queryString = "INSERT INTO permissions (role_id, component_id, permission_id) VALUES\
+                (%s,(SELECT id FROM components WHERE component=%s),%s)"
+            queryParams = []
+            for permission in self.permissions:
+                component = permission['component']
+                try:
+                    permissionId = self.getPermissionId(permission['permission'])
+                except ValueError as e:
+                    print("Error in Get Permission", e)
+                    continue
+                queryParams.append( (roleId, component, permissionId, ) )
+            try:
+                if len(queryParams) > 1:
+                    lastRowId = self.dbhelper.transactmany(queryString, queryParams)
                 else:
-                    rowId = self.dbhelper.transact(queryString_2, queryParams_2[0])
+                    rowId = self.dbhelper.transact(queryString, queryParams[0])
             except Exception as e:
+                print("Error in save permission", e)
                 return { "msg": "Failed to add permission for role " + self.role },403
             return { "msg": "Permission updation successful" },200
         return ROLE_EMPTY
+
+    def getRoleId(self):
+        if self.role:
+            queryString = "SELECT id FROM roles WHERE role=%s"
+            queryParams = (self.role, )
+            cur = self.dbhelper.query(queryString, queryParams)
+            data = cur.fetchone()
+            if data:
+                return data['id']
+            else:
+                queryString1 = "INSERT INTO roles (role) VALUES(%s)"
+                try:
+                    return self.dbhelper.transact(queryString1, queryParams)
+                except Exception:
+                    raise ValueError("Cannot create role from given data!")
+        raise ValueError("Cannot find role from given data!")
+
+    def getPermissionId(self, permission = None):
+        if permission:
+            queryString = "SELECT id FROM permission_def WHERE permission=%s"
+            queryParams = (permission, )
+            cur = self.dbhelper.query(queryString, queryParams)
+            data = cur.fetchone()
+            if data:
+                return data['id']
+            else:
+                queryString1 = "INSERT INTO permission_def (permission) VALUES(%s)"
+                try:
+                    self.dbhelper.transact(queryString1, queryParams)
+                except Exception:
+                    raise ValueError("Cannot create permission from given data!")
+        raise ValueError("Cannot find permission from given data!")
 
     def remove(self, role = None):
         self.dbhelper = DatabaseHelper()
