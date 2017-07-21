99eef2e4f675758ee01820b3a8787fa82e82585f
Progyan Bhattacharya <progyanb@acm.org>
2017-07-21 17:37:16 +0530

    RoleModel: Removed database key dependency
    * Removed SQL statements INSERT on Unique UPDATE
    * Added SELECT if exists INSERT, else UPDATE
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- Models/UserPermission.py
+++ Models/UserPermission.py
@@ -53,15 +53,18 @@ class UserPermission(object):
             return PERMISSION_EMPTY
         return dataList
 
-    def post(self, entry = None, delete = None):
+    def post(self, entry = None, add = True):
         if entry:
-            print("\nData recieved via", "DELETE" if delete else "POST", "in permissions:", entry)
+            print("\nData recieved via", "POST" if add else "DELETE", "in permissions:", entry)
             self.role = entry['role']
             self.data = entry['components']
-            roleId = self.getRoleId()
+            roleId = self.getRoleId(True)
             if not roleId:
                 roleId = self.setRoleId(True)
-                print("New Role Added.")
+                if roleId:
+                    print("New Role", self.role, "Added.")
+                else:
+                    return { "msg": "Failed to add role " + self.role},402
             for item in self.data:
                 component = item['component']
                 componentId = self.getComponentId(component)
@@ -70,16 +73,16 @@ class UserPermission(object):
                     continue
                 permissions = item['permissions']
                 for permission in permissions:
-                    permissionId = self.getPermissionId(permission['permission'], component);
-                    if permissionId:
-                        Permissionflag = permission['permission_id']
-                        Queryflag = not delete and Permissionflag
-                        rowId = self.setPermission(roleId, componentId, permissionId, Queryflag)
-                        if not rowId:
-                            print("Error occured while updating permissions")
-                    else:
-                        print("Invalid permissions given against", component)
-                        continue
+                    Permissionflag = permission['permission_id']
+                    if Permissionflag:
+                        permissionId = self.getPermissionId(permission['permission'], Permissionflag)
+                        if permissionId:
+                            rowId = self.setPermission(roleId, componentId, permissionId, add)
+                            if not rowId:
+                                print("Error occured while updating permissions")
+                        else:
+                            print("Invalid permissions given against", component)
+                            continue
             return { "msg": "Permission update successful." },200
         else:
             return ROLE_EMPTY
@@ -88,16 +91,18 @@ class UserPermission(object):
         if role:
             data = self.get(role)
             if data:
-                res = self.post(data, True)
+                res = self.post(data, False)
                 rowId = self.setRoleId(False)
                 if not rowId:
                     print("Error occured while deleting role:", role)
                 return res
         return ROLE_EMPTY
 
-    def getRoleId(self):
+    def getRoleId(self, checkInUse = None):
         if self.role:
-            queryString = "SELECT id FROM roles WHERE role=%s and in_use='Y'"
+            queryString = "SELECT id FROM roles WHERE role=%s"
+            if checkInUse:
+                queryString += " AND in_use='Y'"
             cur = self.dbhelper.query(queryString, (self.role, ))
             data = cur.fetchone()
             if data:
@@ -106,11 +111,20 @@ class UserPermission(object):
 
     def setRoleId(self, inUseFlag = None):
         # Need to add who added it
+        queryString = None
+        queryParams = ()
         inUse = 'Y' if inUseFlag else 'N'
-        queryString = "INSERT INTO roles (role, in_use) VALUES(%s,%s) ON DUPLICATE KEY UPDATE in_use=%s"
+        roleId = self.getRoleId(False)
+        if roleId:
+            queryString = "UPDATE roles SET in_use=%s WHERE id=%s"
+            queryParams = (inUse, roleId, )
+        else:
+            queryString = "INSERT INTO roles (role, in_use) VALUES(%s,%s)"
+            queryParams = (self.role, inUse, )
         try:
-            rowId = self.dbhelper.transact(queryString, (self.role, inUse, inUse ))
+            rowId = self.dbhelper.transact(queryString, queryParams)
             self.dbhelper.commit()
+            print(rowId)
             return rowId
         except Exception:
             return False
@@ -139,9 +153,16 @@ class UserPermission(object):
         # Get user id from token
         inUse = 'Y' if flag else 'N'
         if roleId and componentId and permissionId:
-            queryString = "INSERT INTO permissions (role_id, component_id, permission_id, in_use) VALUES\
-                (%s,%s,%s,%s) ON DUPLICATE KEY UPDATE in_use=%s"
-            queryParams = (roleId, componentId, permissionId, inUse, inUse )
+            queryString = "SELECT id FROM permissions WHERE role_id=%s AND component_id=%s AND permission_id=%s"
+            queryParams = (roleId, componentId, permissionId, )
+            cur = self.dbhelper.query(queryString, queryParams)
+            data = cur.fetchone()
+            if data:
+                queryString = "UPDATE permissions SET in_use=%s WHERE id=%s"
+                queryParams = (inUse, data['id'], )
+            else:
+                queryString = "INSERT INTO permissions (role_id, component_id, permission_id, in_use) VALUES (%s,%s,%s,%s)"
+                queryParams = (roleId, componentId, permissionId, inUse, )
             try:
                 rowId = self.dbhelper.transact(queryString, queryParams)
                 self.dbhelper.commit()
