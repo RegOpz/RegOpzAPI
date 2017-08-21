337ea27896172fa226c34a461410e328c45e015b
Progyan Bhattacharya <progyanb@acm.org>
2017-08-21 14:58:56 +0530

    RoleModel: Added Auditing to Update/Insert and Delete
    Using Token and AuditHelper
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- Controllers/RoleController.py
+++ Controllers/RoleController.py
@@ -1,13 +1,16 @@
 from flask_restful import Resource,request
 from Models.UserPermission import UserPermission
+from Models.Token import Token
 
 class RoleController(Resource):
     def get(self, role = None):
         return UserPermission().get(role)
 
     def post(self):
+        userId = Token().authenticate()
         res = request.get_json(force=True)
-        return UserPermission().post(res)
+        return UserPermission(userId).post(res)
 
     def delete(self, role = None):
-        return UserPermission().delete(role)
+        userId = Token().authenticate()
+        return UserPermission(userId).delete(role)
--- Models/UserPermission.py
+++ Models/UserPermission.py
@@ -1,9 +1,12 @@
 from Helpers.DatabaseHelper import DatabaseHelper
+from Helpers.AuditHelper import AuditHelper
 from Constants.Status import *
 
 class UserPermission(object):
-    def __init__(self):
+    def __init__(self, userId = None):
         self.dbhelper = DatabaseHelper()
+        self.audit = AuditHelper("def_change_log")
+        self.user_id = userId
 
     def get(self, roleId = None):
         print("\nRecieved GET request in Permissions")
@@ -26,7 +29,7 @@ class UserPermission(object):
                 queryString = queryString.replace("JOIN","LEFT JOIN")
             compQuery = self.dbhelper.query(queryString, queryParams)
             components = compQuery.fetchall()
-            print(components)
+            # print(components)
             componentList = []
             for component in components:
                 queryString = "SELECT p.permission_id,pd.permission FROM permission_def pd \
@@ -56,7 +59,7 @@ class UserPermission(object):
 
     def post(self, entry = None, add = True):
         if entry:
-            print("\nData recieved via", "POST" if add else "DELETE", "in permissions:", entry)
+            print("\nData recieved via", "POST" if add else "DELETE", "in permissions:") #, entry)
             self.role = entry['role']
             self.data = entry['components']
             roleId = self.getRoleId(True)
@@ -67,23 +70,24 @@ class UserPermission(object):
                 else:
                     return { "msg": "Failed to add role " + self.role},402
             for item in self.data:
-                component = item['component']
-                componentId = self.getComponentId(component)
+                self.component = item['component']
+                componentId = self.getComponentId(self.component)
                 if not componentId:
                     print("Invalid component specified.")
                     continue
                 permissions = item['permissions']
                 for permission in permissions:
                     add = True if permission['permission_id'] else False
-                    permissionId = self.getPermissionId(permission['permission'],componentId)
+                    self.permission = permission['permission']
+                    permissionId = self.getPermissionId(self.permission, componentId)
                     if permissionId:
                         rowId = self.setPermission(roleId, componentId, permissionId, add)
                         if not rowId:
-                            print("Error occured while updating permissions")
+                            print("Error occured while updating permission", self.permission, "on", self.component)
                     else:
-                        print("Invalid permissions given against", component)
+                        print("Invalid permissions given against", self.component)
                         continue
-            return { "msg": "Permission update successful." },200
+            return { "msg": "Permission update successful for " + self.role },200
         else:
             return ROLE_EMPTY
 
@@ -109,24 +113,28 @@ class UserPermission(object):
                 return data['id']
             return False
 
-    def setRoleId(self, inUseFlag = None):
-        # Need to add who added it
-        queryString = None
-        queryParams = ()
-        inUse = 'Y' if inUseFlag else 'N'
-        roleId = self.getRoleId(False)
-        if roleId:
-            queryString = "UPDATE roles SET in_use=%s WHERE id=%s"
-            queryParams = (inUse, roleId, )
+    def setRoleId(self, dml_flag = False):
+        dml = "INSERT" if dml_flag else "DELETE"
+        audit_info = {
+            "table_name": "roles",
+            "change_type": dml,
+            "comment": "Nothing",
+            "change_reference": "Role: " + self.role,
+            "maker": self.user_id
+        }
+        id = self.getRoleId(False)
+        if dml == "INSERT":
+            if not id:
+                queryString = "INSERT INTO roles (role, dml_allowed, in_use) VALUES(%s,'N','N')"
+                queryParams = (self.role, )
+                try:
+                    id = self.dbhelper.transact(queryString, queryParams)
+                    self.dbhelper.commit()
+                except Exception:
+                    return False
+            return self.audit.audit_insert({ "audit_info": audit_info }, id)
         else:
-            queryString = "INSERT INTO roles (role, in_use) VALUES(%s,%s)"
-            queryParams = (self.role, inUse, )
-        try:
-            rowId = self.dbhelper.transact(queryString, queryParams)
-            self.dbhelper.commit()
-            return rowId
-        except Exception:
-            return False
+            return self.audit.audit_delete({ "audit_info": audit_info }, id)
 
     def getComponentId(self, component = None):
         if component:
@@ -148,24 +156,31 @@ class UserPermission(object):
                 return data['id']
         return False
 
-    def setPermission(self, roleId = None, componentId = None, permissionId = None, flag = None):
-        # Get user id from token
-        inUse = 'Y' if flag else 'N'
+    def setPermission(self, roleId = None, componentId = None, permissionId = None, dml_flag = None):
+        dml = "INSERT" if dml_flag else "DELETE"
         if roleId and componentId and permissionId:
+            audit_info = {
+                "table_name": "permissions",
+                "change_type": dml,
+                "comment": "Nothing",
+                "change_reference": "Role: " + self.role + " Permission of " + self.permission + " on component " + self.component,
+                "maker": self.user_id
+            }
             queryString = "SELECT id FROM permissions WHERE role_id=%s AND component_id=%s AND permission_id=%s"
             queryParams = (roleId, componentId, permissionId, )
             cur = self.dbhelper.query(queryString, queryParams)
             data = cur.fetchone()
-            if data:
-                queryString = "UPDATE permissions SET in_use=%s WHERE id=%s"
-                queryParams = (inUse, data['id'], )
+            id = data['id'] if data else None
+            if dml == "INSERT":
+                if not id:
+                    queryString = "INSERT INTO permissions (role_id, component_id, permission_id, dml_allowed, in_use) VALUES (%s,%s,%s,'N','N')"
+                    queryParams = (roleId, componentId, permissionId, )
+                    try:
+                        id = self.dbhelper.transact(queryString, queryParams)
+                        self.dbhelper.commit()
+                    except Exception:
+                        return False
+                return self.audit.audit_insert({ "audit_info": audit_info }, id)
             else:
-                queryString = "INSERT INTO permissions (role_id, component_id, permission_id, in_use) VALUES (%s,%s,%s,%s)"
-                queryParams = (roleId, componentId, permissionId, inUse, )
-            try:
-                rowId = self.dbhelper.transact(queryString, queryParams)
-                self.dbhelper.commit()
-                return data['id'] if data else rowId
-            except Exception:
-                return False
+                return self.audit.audit_delete({ "audit_info": audit_info }, id)
         return False
