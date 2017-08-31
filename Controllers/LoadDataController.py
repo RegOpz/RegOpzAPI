cd463c3c9c7fb0fc1aafb7ccbe8c205c139e6084
suchibrata <mailto.dutta.joy@gmail.com>
2017-08-31 21:02:47 +0530

    Enhance LoadDataController
    Delete data on previous status of FAILED in data_catalog
    Add data_catalog entry at the completion of data loading
    Change reference to comp_agg_ref to comp_agg_rule in GenerateReportController
    
    Fixes:#60
    
    Signed-off-by:Suchibrata

--- Controllers/LoadDataController.py
+++ Controllers/LoadDataController.py
@@ -2,6 +2,7 @@ from flask_restful import Resource,abort
 from flask import Flask, request, redirect, url_for
 from Helpers.DatabaseHelper import DatabaseHelper
 import pandas as pd
+from datetime import datetime
 
 UPLOAD_FOLDER='./uploads/source-files/'
 class LoadDataController(Resource):
@@ -16,13 +17,18 @@ class LoadDataController(Resource):
     def load_data(self,source_id,data_file,business_date):
 
         table_name=self.get_table_name(source_id)
+
         if self.check_if_data_exists(source_id,business_date):
             abort(400,error_message="Data already loaded for the given business date")
+        else:
+            self.delete_existing_data(table_name,business_date)
 
         fs=self.get_file_seperator(source_id)
         col_list=self.get_col_list(table_name)
         chunksize=10
 
+        num_records=0
+        file_load_status='SUCCESS'
         for chunk in pd.read_csv(UPLOAD_FOLDER+data_file,sep=fs,index_col=0,chunksize=chunksize,na_filter=False):
            #print(chunk)
            found_col_list=[]
@@ -48,21 +54,42 @@ class LoadDataController(Resource):
            placeholders += ")"
            sql = sql[:len(sql) - 1]
            sql += ") values " + placeholders
-           print(sql)
-           print(params_tuple)
+           #print(sql)
+           #print(params_tuple)
+           header_row = chunk.columns.values
+           try:
+              res= self.db.transactmany(sql, params_tuple)
+              self.db.commit()
+              num_records += chunk.shape[0]
+              print("Another "+chunk.shape[0]+" records uploaded....")
+           except Exception as e:
+              file_load_status='FAILED'
+              #print(e)
+              break
 
-           res= self.db.transactmany(sql, params_tuple)
+        self.create_data_catalog(source_id,business_date,data_file,num_records,header_row,file_load_status)
+        return res
 
+    def create_data_catalog(self,source_id,business_date,file_name,number_of_rows,header_row,file_load_status):
+        sql="insert into data_catalog(source_id,business_date,data_file_name,number_of_rows,file_load_status,\
+        header_row,data_loaded_by,timestamp) values(%s,%s,%s,%s,%s,%s,%s,%s)"
+        params=(source_id,business_date,file_name,number_of_rows,file_load_status,str(header_row),None,datetime.now())
+
+        res=self.db.transact(sql,params)
         return res
 
 
+    def delete_existing_data(self,table_name,business_date):
+        sql="delete from "+table_name+" where business_date=%s"
+        self.db.transact(sql,(str(business_date),))
+        self.db.commit()
 
 
     def check_if_data_exists(self,source_id,business_date):
-        sql="select * from data_catalog where business_date=%s and source_id=%s and file_load_status=%s"
-        file=self.db.query(sql,(business_date,source_id,'SUCCESS')).fetchone()
+        sql="select * from data_catalog where business_date=%s and source_id=%s"
+        file=self.db.query(sql,(business_date,source_id)).fetchone()
 
-        return True if file else False
+        return True if file and file['file_load_status']=='SUCCESS' else False
 
     def get_file_seperator(self,source_id):
         sql="select * from data_source_information where source_id=%s"
--- Helpers/CustomMySQLConverter.py
+++ Helpers/CustomMySQLConverter.py
@@ -3,3 +3,5 @@ import mysql.connector
 class CustomMySQLConverterClass(mysql.connector.conversion.MySQLConverter):
     def _int64_to_mysql(self,value):
         return int(value)
+    def _ndarray_to_mysql(self,value):
+        return str(value)
