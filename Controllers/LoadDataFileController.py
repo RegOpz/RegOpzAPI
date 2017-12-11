b909581b6b90de96ff27c093ebe5b19f4ea3c79c
RegOpz <debmalya.barman@regopz.com>
2017-12-11 12:38:57 +0530

    Data File Load status value check added

--- Controllers/LoadDataFileController.py
+++ Controllers/LoadDataFileController.py
@@ -1,3 +1,4 @@
+from app import *
 import os
 import uuid
 from datetime import datetime
@@ -11,6 +12,7 @@ UPLOAD_FOLDER = './uploads/source-files'
 class LoadDataFileController(Resource):
     def post(self):
         file = request.files['data_file']
+        app.logger.info("Inside post of Loading data {}".format(file))
         selectedfilename = secure_filename(file.filename)
         if file:
             filename = str(uuid.uuid4()) + '_' +\
@@ -18,4 +20,6 @@ class LoadDataFileController(Resource):
                     ':', '_') + '_' + secure_filename(file.filename)
 
             file.save(os.path.join(UPLOAD_FOLDER, filename).replace('\\', '/'))
-            return {'msg': 'File Transferred Successfully [' + selectedfilename + ']', 'filename': filename}, 200
+            return {'msg': 'File Transferred Successfully [' + selectedfilename + ']',
+                    'filename': filename,
+                    'status': 'SUCCESS'}, 200
