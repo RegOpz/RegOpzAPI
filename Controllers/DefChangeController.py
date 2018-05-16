5ff0cb498c56944b5d6364a5e1a723a7e65977a0
RegOpz <debmalya.barman@regopz.com>
2018-05-16 11:54:56 +0530

    Changes for group approval and submission of changes

--- Controllers/DefChangeController.py
+++ Controllers/DefChangeController.py
@@ -4,7 +4,6 @@ from flask_restful import Resource
 from Helpers.DatabaseHelper import DatabaseHelper
 from Helpers.DatabaseOps import DatabaseOps
 from Helpers.AuditHelper import AuditHelper
-import json
 from Helpers.utils import autheticateTenant
 from Helpers.authenticate import *
 import pandas as pd
@@ -29,7 +28,9 @@ class DefChangeController(Resource):
         if request.endpoint=="get_audit_list":
             table_name=request.args.get("table_name")
             id_list=request.args.get("id_list")
-            return self.get_audit_list(id_list,table_name)
+            if table_name and table_name!='undefined':
+                return self.get_audit_history(id_list,table_name)
+            return self.get_audit_list()
         if request.endpoint=="get_record_detail":
             table_name=request.args.get("table_name")
             id=request.args.get("id")
@@ -39,12 +40,12 @@ class DefChangeController(Resource):
 
     #@authenticate
     def post(self):
-        data=request.get_json(force=True)
-        return self.audit_decision(data)
+        data_list=request.get_json(force=True)
+        return self.audit_decision(data_list)
 
 
 
-    def get_audit_list(self,id_list=None,table_name=None):
+    def get_audit_list(self):
         app.logger.info("Getting audit list")
         try:
             pending_audit_df=pd.DataFrame(self.db.query("select * from def_change_log where status='PENDING'").fetchall())
@@ -58,9 +59,9 @@ class DefChangeController(Resource):
                 deletes = 0
                 updates = 0
                 df=pending_audit_df.loc[pending_audit_df['group_id']==grp]
-                maker=pending_audit_df['maker'].unique()[0]
-                maker_tenant_id=pending_audit_df['maker_tenant_id'].unique()[0]
-                checker_tenant_id=pending_audit_df['checker_tenant_id'].unique()[0]
+                maker=df['maker'].unique()[0]
+                maker_tenant_id=df['maker_tenant_id'].unique()[0]
+                checker_tenant_id=df['checker_tenant_id'].unique()[0]
                 group_tables=df['table_name'].unique()
                 group_date_of_change=df['date_of_change'].min()
                 non_update_df=df.loc[df['change_type']!='UPDATE']
@@ -119,11 +120,39 @@ class DefChangeController(Resource):
             app.logger.error(str(e))
             return {"msg":str(e)},500
 
+    def get_audit_history(self,id_list=None,table_name=None):
+        app.logger.info("Getting audit change history list")
+        try:
+            sql = "SELECT DISTINCT id,table_name,change_type,change_reference," + \
+                    "date_of_change,maker,maker_comment,checker,checker_comment,status,date_of_checking " + \
+                    "FROM def_change_log WHERE id IN (" + \
+                    (id_list if id_list and id_list!='undefined' else "id") + \
+                    ") AND table_name = '" + table_name + "'"
+
+            audit_hist = self.db.query(sql).fetchall()
+            # app.logger.info("Audit hist list {}".format(audit_hist,))
+            for i,item in enumerate(audit_hist):
+                for k,v in item.items():
+                    if isinstance(v,datetime):
+                        item[k] = item[k].isoformat()
+                if item["change_type"]=="UPDATE":
+                    values=self.db.query("select field_name,old_val,new_val from def_change_log where id="+str(item["id"])+
+                                         " and table_name='"+str(item["table_name"])+"' and status='"+str(item["status"])+
+                                         "' and date_of_change='"+str(item["date_of_change"])+"'").fetchall()
+                    update_info_list=[]
+                    for val in values:
+                        update_info_list.append({"field_name":val["field_name"],"old_val":val["old_val"],"new_val":val["new_val"]})
+                    audit_hist[i]["update_info"]=update_info_list
+
+            return audit_hist
+
+        except Exception as e:
+            app.logger.error(e)
+            return {"msg":e},500
+
     def get_record_detail(self,table_name,id):
         app.logger.info("Getting record detail")
         try:
-            if table_name=="business_rules":
-                return self.fetch_business_rule_detail(id)
             if table_name:
                 record = self.db.query("select * from {} where id=%s".format(table_name,),(id,)).fetchone()
                 return record
@@ -134,26 +163,25 @@ class DefChangeController(Resource):
             app.logger.error(str(e))
             return {"msg":str(e)},500
 
-    def fetch_business_rule_detail(self,id):
-        app.logger.info("Getting record detail for business rules id:{}".format(id))
-        try:
-            business_rule_detail=self.db.query("select * from business_rules where id=%s",(id,)).fetchone()
-            return business_rule_detail
-        except Exception as e:
-            app.logger.error(str(e))
-            raise e
-
     def audit_decision(self,data_list):
         try:
-            app.logger.info("Inside audit decision for status {}".format(data["status"]))
-            for data in data_list:
-                if data["status"]=="REJECTED":
-                    self.audit.reject_dml(data)
-                if data["status"]=="APPROVED":
-                    self.audit.approve_dml(data)
-                if data["status"]=="REGRESSED":
-                    self.audit.regress_dml(data)
-            return {"msg":"Successfully changed status for the records"},200
+            approve=0
+            reject=0
+            regress=0
+            for key in data_list.keys():
+                for data in data_list[key]:
+                    app.logger.info("Inside audit decision for status {}".format(data["status"]))
+                    if data["status"]=="REJECTED":
+                        self.audit.reject_dml(data)
+                        reject+=1
+                    if data["status"]=="APPROVED":
+                        self.audit.approve_dml(data)
+                        approve+=1
+                    if data["status"]=="REGRESSED":
+                        self.audit.regress_dml(data)
+                        regress+=1
+            return {"msg":"Successfully processed reviews for the following: " + \
+                          "{0} approval, {1} rejection and {2} regress".format(approve,reject,regress)},200
         except Exception as e:
             app.logger.error(str(e))
             return {"msg": str(e)},500
