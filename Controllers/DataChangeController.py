663efe9f79595c10b4fb7108385fb9db5c240d68
RegOpz <debmalya.barman@regopz.com>
2018-05-24 09:15:15 +0530

    Moved dbOps and AuditHelper methods to datachange controller
    dbOps and Audithelper methods now part of data change controller
    Implemented changes for new versioning and audit approach for data change controller

--- Controllers/DataChangeController.py
+++ Controllers/DataChangeController.py
@@ -2,8 +2,6 @@ from app import *
 from flask import Flask, jsonify, request
 from flask_restful import Resource
 from Helpers.DatabaseHelper import DatabaseHelper
-from Helpers.DatabaseOps import DatabaseOps
-from Helpers.AuditHelper import AuditHelper
 from Helpers.utils import autheticateTenant
 from Helpers.authenticate import *
 import pandas as pd
@@ -16,7 +14,6 @@ class DataChangeController(Resource):
         if self.domain_info:
             tenant_info = json.loads(self.domain_info)
             self.tenant_info = json.loads(tenant_info['tenant_conn_details'])
-            self.audit=AuditHelper('data_change_log',self.tenant_info)
             self.db=DatabaseHelper(self.tenant_info)
 
     # @authenticate
@@ -39,20 +36,33 @@ class DataChangeController(Resource):
 
 
     def get_audit_history(self,id_list = None,table_name = None,business_date = None):
-        app.logger.info("Getting audit list")
+        app.logger.info("Getting audit list history")
         try:
-            sql = "SELECT DISTINCT id,table_name,change_type,change_reference,\
-                date_of_change,maker,maker_comment,checker,checker_comment,status,date_of_checking,\
-                business_date FROM {} WHERE 1"
+            sql = "SELECT * FROM data_change_log WHERE 1"
             if id_list == "id" or ((id_list is None or id_list == 'undefined') and (table_name is None or table_name=='undefined')):
                 sql += " AND status='PENDING'"
             if id_list:
-                sql = sql + " and (id,business_date) in (" + id_list + ") "
+                sql += " and (origin_id,business_date) in (" + \
+                            "select origin_id,business_date from data_change_log " + \
+                            "where (id,business_date) in (" + id_list + ")" + \
+                            ")"
+                # sql = sql + " and (id,business_date) in (" + id_list + ") "
             elif business_date:
                 sql = sql + " and business_date=" + business_date
             if table_name is not None and table_name != 'undefined':
                 sql = sql + " and table_name = '" + table_name + "'"
-            return self.audit.get_audit_list(sql)
+
+            audit_list = self.db.query(sql).fetchall()
+            for i,d in enumerate(audit_list):
+                # app.logger.info('Processing index {}'.format(i))
+                if d["change_type"]=='UPDATE':
+                    d['update_info']=json.loads(d['change_summary'])
+                for k,v in d.items():
+                    if isinstance(v,datetime):
+                        d[k] = d[k].isoformat()
+                        # app.logger.info("{0} {1}".format(d[k], type(d[k])))
+            return audit_list
+
         except Exception as e:
             app.logger.error(e)
             return {"msg":e},500
@@ -88,26 +98,24 @@ class DataChangeController(Resource):
                 if not update_df.empty:
                     update_list = []
 
-                    for idx,ugrp in update_df.groupby(['table_name','id']):
-                        update_info = []
+                    for idx,ugrp in update_df.iterrows():
+                        update_info = json.loads(ugrp['change_summary'])
                         updates += 1
-                        for ix, row in ugrp.iterrows():
-                            update_info.append(
-                                {"field_name": row['field_name'], "old_val": row["old_val"], "new_val": row["new_val"]})
-
-                        update_list.append({"id": str(ugrp['id'].unique()[0]),
-                                            "table_name": ugrp['table_name'].unique()[0],
-                                            "change_type": ugrp['change_type'].unique()[0],
-                                            "date_of_change": ugrp['date_of_change'].unique()[0],
-                                            "maker": ugrp['maker'].unique()[0],
-                                            "maker_comment": ugrp['maker_comment'].unique()[0],
-                                            "checker": ugrp['checker'].unique()[0],
-                                            "checker_comment": ugrp['checker_comment'].unique()[0],
-                                            "status": ugrp['status'].unique()[0],
-                                            "date_of_checking": ugrp['date_of_checking'].unique()[0],
-                                            "change_reference": ugrp['change_reference'].unique()[0],
-                                            "group_id": str(ugrp['group_id'].unique()[0]),
-                                            "business_date": int(ugrp['business_date'].unique()[0]),
+                        update_list.append({"id": str(ugrp['id']),
+                                            "prev_id": str(ugrp['prev_id']),
+                                            "origin_id": str(ugrp['origin_id']),
+                                            "table_name": ugrp['table_name'],
+                                            "change_type": ugrp['change_type'],
+                                            "date_of_change": ugrp['date_of_change'],
+                                            "maker": ugrp['maker'],
+                                            "maker_comment": ugrp['maker_comment'],
+                                            "checker": ugrp['checker'],
+                                            "checker_comment": ugrp['checker_comment'],
+                                            "status": ugrp['status'],
+                                            "date_of_checking": ugrp['date_of_checking'],
+                                            "change_reference": ugrp['change_reference'],
+                                            "group_id": str(ugrp['group_id']),
+                                            "business_date": int(ugrp['business_date']),
                                             "update_info": update_info
                                             })
 
@@ -139,6 +147,246 @@ class DataChangeController(Resource):
             app.logger.error(e)
             return {"msg":e},500
 
+    def update_or_delete_data(self,data,id):
+        if data['change_type']=='DELETE':
+            return self.delete_data(data,id)
+        if data['change_type']=='UPDATE':
+            return self.update_data(data,id)
+
+    def create_new_record(self, data):
+        try:
+            table_name = data['table_name']
+            change_type = data['change_type']
+            app.logger.info("Inserting record for : {0} Change Event: {1}".format(table_name,change_type))
+
+            update_info = data['update_info']
+            # If id value is present set it to null
+            update_info['id']=None
+            update_info['dml_allowed']='N'
+            update_info['in_use']='N'
+            update_info_cols = ",".join(update_info.keys())
+            update_info_placeholder = ",".join(["%s"] * len(update_info.keys()))
+            params_tuple = tuple(update_info.values())
+
+            sql = "insert into {0} ({1}) values ({2})".format(table_name,update_info_cols,update_info_placeholder)
+            app.logger.info("Inserting new record: {0} {1}".format(sql,params_tuple))
+            id = self.db.transact(sql, params_tuple)
+
+            return id
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+    def insert_data(self, data):
+        try:
+            app.logger.info("Insert data for source")
+            table_name = data['table_name']
+            change_type = data['change_type']
+            id = self.create_new_record(data)
+            self.audit_insert(data,id)
+            self.db.commit()
+
+            return {"msg": "Inserted record related version and audit data created for " + \
+                            "{0} ID: {1}".format(table_name, id)},200
+        except Exception as e:
+            app.logger.error(str(e))
+            self.db.rollback()
+            return {"msg": str(e)},500
+
+    def update_data(self, data, prev_id):
+        try:
+            app.logger.info("Update data for source table")
+            table_name = data['table_name']
+            change_type = data['change_type']
+
+            id = self.create_new_record(data)
+            self.audit_update(data,id,prev_id)
+            self.update_approval_status(table_name=table_name, id=prev_id, dml_allowed='X')
+            self.db.commit()
+
+            return {"msg": "Update record related version and audit data created for " + \
+                            "{0} ID: {1}".format(table_name, prev_id)},200
+        except Exception as e:
+            app.logger.error(str(e))
+            self.db.rollback()
+            return {"msg": str(e)},500
+
+    def delete_data(self, data, prev_id):
+        try:
+            app.logger.info("Delete data for source table")
+            table_name = data['table_name']
+            change_type = data['change_type']
+
+            self.audit_delete(data,prev_id)
+            self.update_approval_status(table_name=table_name, id=prev_id, dml_allowed='N')
+            self.db.commit()
+
+            return {"msg": "Delete record related version and audit data created for " + \
+                            "{0} ID: {1}".format(table_name, prev_id)},200
+        except Exception as e:
+            app.logger.error(str(e))
+            self.db.rollback()
+            return {"msg": str(e)},500
+
+    def audit_insert(self, data, id):
+        try:
+            app.logger.info("Data Change controller audit info insert")
+            audit_info = data['audit_info']
+            sql = "insert into  data_change_log " + \
+            "(id,prev_id,origin_id,table_name,change_type,maker_comment,status,change_reference," + \
+            "date_of_change,maker,business_date,group_id)" + \
+            " values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            res = self.db.transact(sql, (id, id, id, audit_info['table_name'], audit_info['change_type'], \
+                    audit_info['comment'],'PENDING', audit_info['change_reference'], datetime.now(), \
+                    audit_info['maker'],audit_info['business_date'],audit_info['group_id']))
+
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+    def audit_update(self, data, id, prev_id):
+        try:
+            app.logger.info("Data Change controller audit info update")
+            audit_info = data['audit_info']
+            update_info = data['update_info']
+            table_name = data['table_name']
+
+            # Now get the origin id for the record if there is any existing entry in data change log
+            sql="select origin_id from data_change_log where table_name='{0}' and id={1}".format(table_name,prev_id)
+            origin_id=self.db.query(sql).fetchone()
+            # If no previous entry found, that means the record is updated for the very first time, so
+            # use the prev (present) id as origin_id to be casted for subsequent changes
+            if not origin_id:
+                origin_id = prev_id
+            else:
+                origin_id = origin_id['origin_id']
+
+            change_summary = []
+            old_rec=self.db.query("select * from "+ table_name+" where id="+str(prev_id)).fetchone()
+            for col in update_info.keys():
+                old_val = old_rec[col]
+                new_val = update_info[col]
+                if old_val != new_val and col not in ('dml_allowed','in_use','id'):
+                    change_summary.append({'field_name': col, 'old_val': old_val,'new_val':new_val})
+
+            sql = "insert into  data_change_log " + \
+            "(id,prev_id,origin_id,table_name,change_type,maker_comment,status,change_reference," + \
+            "date_of_change,maker,business_date,group_id, change_summary)" + \
+            " values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            res = self.db.transact(sql, (id, prev_id, origin_id, audit_info['table_name'], audit_info['change_type'], \
+                    audit_info['comment'],'PENDING', audit_info['change_reference'], datetime.now(), \
+                    audit_info['maker'],audit_info['business_date'],audit_info['group_id'],
+                    json.dumps(change_summary)))
+
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+    def audit_delete(self, data, prev_id):
+        try:
+            app.logger.info("Data Change controller audit info delete")
+            table_name = data['table_name']
+            audit_info = data['audit_info']
+            # Now get the origin id for the record if there is any existing entry in data change log
+            sql="select origin_id from data_change_log where table_name='{0}' and id={1}".format(table_name,prev_id)
+            origin_id=self.db.query(sql).fetchone()
+            # If no previous entry found, that means the record is updated for the very first time, so
+            # use the prev (present) id as origin_id to be casted for subsequent changes
+            if not origin_id:
+                origin_id = prev_id
+            else:
+                origin_id = origin_id['origin_id']
+
+            sql = "insert into  data_change_log " + \
+            "(id,prev_id,origin_id,table_name,change_type,maker_comment,status,change_reference," + \
+            "date_of_change,maker,business_date,group_id)" + \
+            " values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            res = self.db.transact(sql, (prev_id, prev_id, origin_id, audit_info['table_name'], audit_info['change_type'], \
+                    audit_info['comment'],'PENDING', audit_info['change_reference'], datetime.now(), \
+                    audit_info['maker'],audit_info['business_date'],audit_info['group_id']))
+
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+    def update_approval_status(self,table_name,id,dml_allowed,in_use=None):
+        try:
+            app.logger.info("Inside update approval status for {0} {1}".format(table_name,id))
+            sql = "update " + table_name + " set dml_allowed='"+dml_allowed+"'"
+            if in_use is not None:
+                sql+=",in_use='"+in_use+"' "
+            sql+= "  where id=%s"
+            res = self.db.transact(sql, (id,))
+
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+    def update_audit_record(self,data):
+        try:
+            app.logger.info("Updateing audit record for data audit decision")
+            sql="update data_change_log set status=%s,checker_comment=%s,date_of_checking=%s,checker=%s where table_name=%s and id=%s and status='PENDING'"
+            params=(data["status"],data["checker_comment"],datetime.now(),data["checker"],data["table_name"],data["id"])
+            self.db.transact(sql,params)
+
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+    def reject_dml(self,data):
+        try:
+            app.logger.info("AuditDicision reject dml")
+            if data["change_type"]=="INSERT":
+                self.update_approval_status(table_name=data["table_name"],id=data["id"],dml_allowed='Y',in_use='N')
+            elif data["change_type"]=="UPDATE":
+                self.update_approval_status(table_name=data["table_name"], id=data["prev_id"], dml_allowed='Y')
+                self.update_approval_status(table_name=data["table_name"], id=data["id"], dml_allowed='X',in_use='X')
+            elif data["change_type"]=="DELETE":
+                self.update_approval_status(table_name=data["table_name"], id=data["id"], dml_allowed='Y')
+            self.update_audit_record(data)
+            self.db.commit()
+
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+    def regress_dml(self,data):
+        try:
+            app.logger.info("AuditDicision regress dml")
+            if data["change_type"]=="INSERT":
+                self.update_approval_status(table_name=data["table_name"],id=data["id"],dml_allowed='Y',in_use='N')
+            elif data["change_type"]=="UPDATE":
+                self.update_approval_status(table_name=data["table_name"], id=data["prev_id"], dml_allowed='Y')
+                self.update_approval_status(table_name=data["table_name"], id=data["id"], dml_allowed='X',in_use='X')
+            elif data["change_type"]=="DELETE":
+                self.update_approval_status(table_name=data["table_name"], id=data["id"], dml_allowed='Y')
+            self.update_audit_record(data)
+            self.db.commit()
+
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+
+    def approve_dml(self,data):
+        try:
+            app.logger.info("AuditDicision approval dml")
+            if data["change_type"]=="INSERT":
+                self.update_approval_status(table_name=data["table_name"],id=data["id"],dml_allowed='Y',in_use='Y')
+            elif data["change_type"]=="DELETE":
+                self.update_approval_status(table_name=data["table_name"], id=data["id"], dml_allowed='N',in_use='N')
+            elif data["change_type"] == "UPDATE":
+                self.update_approval_status(table_name=data["table_name"], id=data["prev_id"], dml_allowed='X',in_use='X')
+                self.update_approval_status(table_name=data["table_name"], id=data["id"], dml_allowed='Y',in_use='Y')
+
+            self.update_audit_record(data)
+            self.db.commit()
+
+        except Exception as e:
+            app.logger.error(str(e))
+            raise(e)
+
+
     def audit_decision(self,data_list):
         try:
             approve=0
@@ -148,16 +396,16 @@ class DataChangeController(Resource):
                 for data in data_list[key]:
                     app.logger.info("Inside audit decision for status {}".format(data["status"]))
                     if data["status"]=="REJECTED":
-                        self.audit.reject_dml(data)
+                        self.reject_dml(data)
                         reject+=1
                     if data["status"]=="APPROVED":
-                        self.audit.approve_dml(data)
+                        self.approve_dml(data)
                         approve+=1
                     if data["status"]=="REGRESSED":
-                        self.audit.regress_dml(data)
+                        self.regress_dml(data)
                         regress+=1
             return {"msg":"Successfully processed reviews for the following: " + \
-                          "{1} approval, {2} rejection and {3} regress".format(approve,reject,regress)},200
+                          "{0} approval, {1} rejection and {2} regress".format(approve,reject,regress)},200
         except Exception as e:
             app.logger.error(str(e))
             return {"msg": str(e)},500
